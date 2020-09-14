ad28b9512723e4abc685da13278c8e0c405d8112
PRATHAM18111999 <69888162+PRATHAM18111999@users.noreply.github.com>
2020-09-14 20:52:43 +0530

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -20,10 +20,10 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print "Author   : Tech Tushar"
-print "You Tube : https://www.youtube.com/c/TechTushar"
-print "github   : https://github.com/Techtushar07"
-print "Facebook : https://www.facebook.com/daveratushar/"
+print "Author   : Prathamesh"
+print "You Tube : https://www.youtube.com/c/Prathamesh18111999"
+print "github   :https://github.com/PRATHAM18111999"
+print "Twitter : https://twitter.com/pratham18111999"
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
